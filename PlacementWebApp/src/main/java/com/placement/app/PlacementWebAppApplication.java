d2314963a9aec1c896a154d79200e3ad4af184b1
prasa <prasa@LAPTOP-F0S4H07F>
2023-05-02 08:25:22 +0530

    first commit

---
+++ PlacementWebApp/src/main/java/com/placement/app/PlacementWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.placement.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementWebAppApplication.class, args);
+	}
+
+}
